2aba4e5258ea287b5197252b5239c483e3b0727d
prajwal714 <prajwal714singh@gmail.com>
2019-08-20 14:48:56 +0530

    basic client-server comm

--- server/server.js
+++ server/server.js
@@ -1,18 +1,34 @@
 const path = require("path");
+const http = require("http");
 const publicPath = path.join(__dirname, "../public");
-
+const port = process.env.PORT || 3000;
 const express = require("express");
+const socketIO = require("socket.io");
 
 const app = express();
+const server = http.createServer(app);
+var io=socketIO(server);
+
 
 app.use(express.static(publicPath));
+
+
+io.on('connection',(socket)=>{
+
+    console.log("new User connected...");
+    socket.on('disconnect',()=>{
+        console.log("user disconnected");
+    })
+
+});
+
+
 
 app.get("/", (req, res) => {
   res.send(publicPath + "index.html");
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port, err => {
+server.listen(port, err => {
   if (err) throw err;
 
   console.log(`server started at port ${port}`);
